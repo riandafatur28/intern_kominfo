d694caff6874323d3ecafd12c1266b243184e775
Rianda <e41231605@student.polije.ac.id>
2026-08-08 16:28:26 +0700

    chore(routes): samakan routes/api.php dengan features/backend (hapus route deleteSignature)

--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,6 @@ Route::middleware(['auth:sanctum', 'password.changed'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'show']);
     Route::put('/profile', [ProfileController::class, 'update']);
     Route::post('/profile/signature', [ProfileController::class, 'uploadSignature']);
-    Route::delete('/profile/signature', [ProfileController::class, 'deleteSignature']);
 
     // Teams (dropdown options)
     Route::get('/teams', [TeamController::class, 'index']);
